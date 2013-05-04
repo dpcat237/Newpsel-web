f6493f1c6baef0b2c2b21580fa5656c260cbdf0d
Denys Pasishnyi <dpcat237@gmail.com>
2013-05-04 19:14:39 +0200

    try receive json

--- src/NPS/ApiBundle/Controller/UserController.php
+++ src/NPS/ApiBundle/Controller/UserController.php
@@ -20,13 +20,19 @@ class UserController extends BaseController
      */
     public function loginAction(Request $request)
     {
+        $json = json_decode($request->getContent());
+        echo '<pre>tut: '; print_r($json); echo '</pre>'; exit();
+
+
+
+
         //echo 'tut: '.$request->getMethod();
-        echo '<pre>tut1: '; print_r($_FILES); echo '</pre>';
+        /*echo '<pre>tut1: '; print_r($_FILES); echo '</pre>';
         \Doctrine\Common\Util\Debug::dump($_POST);
         echo '<pre>tut2: '; print_r($request->getContent()); echo '</pre>';
         $json = $request->request->get('JSONFile');
         $jsonData = json_decode($json);
-        echo '<pre>tut3: '; print_r($jsonData); echo '</pre>'; exit();
+        echo '<pre>tut3: '; print_r($jsonData); echo '</pre>'; exit();*/
     }
 
 }
