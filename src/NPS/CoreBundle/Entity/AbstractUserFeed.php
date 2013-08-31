d54ddb149b6fc6a05ebff95d2eafb5fccf9bc23d
Denys Pasishnyi <dpcat237@gmail.com>
2013-08-31 11:48:34 +0000

    Refactory User class

---
+++ src/NPS/CoreBundle/Entity/AbstractUserFeed.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace NPS\CoreBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use NPS\CoreBundle\Entity\UserItem;
+use NPS\CoreBundle\Entity\UserFeed;
+use NPS\CoreBundle\Entity\AbstractUser;
+
+/**
+ * AbstractUserFeed
+ *
+ */
+abstract class AbstractUserFeed extends AbstractUser
+{
+    /**
+     * @ORM\OneToMany(targetEntity="UserItem", mappedBy="user")
+     */
+    protected $userItems;
+
+    /**
+     * @ORM\OneToMany(targetEntity="UserFeed", mappedBy="user")
+     */
+    protected $userFeeds;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Later", mappedBy="user")
+     */
+    protected $laters;
+
+    /**
+     * @var boolean
+     * @ORM\Column(name="subscribed", type="boolean")
+     */
+    protected $subscribed = false;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->userItems = new ArrayCollection();
+        $this->userFeeds = new ArrayCollection();
+        $this->laters = new ArrayCollection();
+    }
+
+    /**
+     * Add userItem
+     * @param UserItem $userItem
+     *
+     * @return User
+     */
+    public function addUserItem(UserItem $userItem)
+    {
+        $this->userItems[] = $userItem;
+
+        return $this;
+    }
+
+    /**
+     * Remove userItem
+     *
+     */
+    public function removeUserItem(UserItem $userItem)
+    {
+        $this->userItems->removeElement($userItem);
+    }
+
+    /**
+     * Get userItems
+     *
+     * @return Collection
+     */
+    public function getUserItems()
+    {
+        return $this->userItems;
+    }
+
+
+    /**
+     * Add userFeed
+     * @param UserFeed $userFeed
+     *
+     * @return User
+     */
+    public function addUserFeed(UserFeed $userFeed)
+    {
+        $this->userFeeds[] = $userFeed;
+
+        return $this;
+    }
+
+    /**
+     * Remove userFeed
+     *
+     */
+    public function removeUserFeed(UserFeed $userFeed)
+    {
+        $this->userFeeds->removeElement($userFeed);
+    }
+
+    /**
+     * Get userFeeds
+     *
+     * @return Collection
+     */
+    public function getUserFeeds()
+    {
+        return $this->userFeeds;
+    }
+
+    /**
+     * Set subscribed
+     * @param \boolean $subscribed
+     *
+     * @return User
+     */
+    public function setSubscribed($subscribed)
+    {
+        $this->subscribed = $subscribed;
+
+        return $this;
+    }
+
+    /**
+     * Is subscribed
+     *
+     * @return \int
+     */
+    public function isSubscribed()
+    {
+        return $this->subscribed;
+    }
+
+    /**
+     * Add Later
+     * @param Later $later
+     *
+     * @return User
+     */
+    public function addLater(Later $later)
+    {
+        $this->laters[] = $later;
+
+        return $this;
+    }
+
+    /**
+     * Remove userFeed
+     *
+     */
+    public function removeLater(Later $later)
+    {
+        $this->laters->removeElement($later);
+    }
+
+    /**
+     * Get Laters
+     *
+     * @return Collection
+     */
+    public function getLaters()
+    {
+        return $this->laters;
+    }
+}
--- src/NPS/CoreBundle/Entity/User.php
+++ src/NPS/CoreBundle/Entity/User.php
@@ -6,9 +6,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use NPS\CoreBundle\Entity\Device;
-use NPS\CoreBundle\Entity\Feed;
-use NPS\CoreBundle\Entity\UserItem;
-use NPS\CoreBundle\Entity\UserFeed;
+use NPS\CoreBundle\Entity\AbstractUserFeed;
 
 /**
  * User
@@ -17,7 +15,7 @@ use NPS\CoreBundle\Entity\UserFeed;
  * @ORM\Table(name="user")
  * @ORM\HasLifecycleCallbacks
  */
-class User extends AbstractUser
+class User extends AbstractUserFeed
 {
     /**
      * @ORM\OneToMany(targetEntity="Device", mappedBy="user")
@@ -25,32 +23,10 @@ class User extends AbstractUser
     protected $devices;
 
     /**
-     * @ORM\OneToMany(targetEntity="UserItem", mappedBy="user")
-     */
-    protected $userItems;
-
-    /**
-     * @ORM\OneToMany(targetEntity="UserFeed", mappedBy="user")
-     */
-    protected $userFeeds;
-
-    /**
      * @var boolean
      * @ORM\Column(name="registered", type="boolean")
      */
     protected $registered = false;
-
-    /**
-     * @var boolean
-     * @ORM\Column(name="subscribed", type="boolean")
-     */
-    protected $subscribed = false;
-
-
-    /**
-     * @ORM\OneToMany(targetEntity="Later", mappedBy="user")
-     */
-    protected $laters;
 
 
     /**
@@ -58,17 +34,15 @@ class User extends AbstractUser
      */
     public function __construct()
     {
+        parent::__construct();
         $this->devices = new ArrayCollection();
-        $this->userItems = new ArrayCollection();
-        $this->userFeeds = new ArrayCollection();
-        $this->laters = new ArrayCollection();
     }
 
     /**
      * Add devices
      * @param Device $device
      *
-     * @return Feed
+     * @return User
      */
     public function addDevice(Device $device)
     {
@@ -98,71 +72,6 @@ class User extends AbstractUser
     }
 
     /**
-     * Add userItem
-     * @param UserItem $userItem
-     *
-     * @return User
-     */
-    public function addUserItem(UserItem $userItem)
-    {
-        $this->userItems[] = $userItem;
-
-        return $this;
-    }
-
-    /**
-     * Remove userItem
-     *
-     */
-    public function removeUserItem(UserItem $userItem)
-    {
-        $this->userItems->removeElement($userItem);
-    }
-
-    /**
-     * Get userItems
-     *
-     * @return Collection
-     */
-    public function getUserItems()
-    {
-        return $this->userItems;
-    }
-
-
-    /**
-     * Add userFeed
-     * @param UserFeed $userFeed
-     *
-     * @return User
-     */
-    public function addUserFeed(UserFeed $userFeed)
-    {
-        $this->userFeeds[] = $userFeed;
-
-        return $this;
-    }
-
-    /**
-     * Remove userFeed
-     *
-     */
-    public function removeUserFeed(UserFeed $userFeed)
-    {
-        $this->userFeeds->removeElement($userFeed);
-    }
-
-    /**
-     * Get userFeeds
-     *
-     * @return Collection
-     */
-    public function getUserFeeds()
-    {
-        return $this->userFeeds;
-    }
-
-    /**
      * Set registered
      * @param \boolean $registered
      *
@@ -183,60 +92,5 @@ class User extends AbstractUser
     public function isRegistered()
     {
         return $this->registered;
-    }
-
-    /**
-     * Set subscribed
-     * @param \boolean $subscribed
-     *
-     * @return User
-     */
-    public function setSubscribed($subscribed)
-    {
-        $this->subscribed = $subscribed;
-
-        return $this;
-    }
-
-    /**
-     * Is subscribed
-     *
-     * @return \int
-     */
-    public function isSubscribed()
-    {
-        return $this->subscribed;
-    }
-
-    /**
-     * Add Later
-     * @param Later $later
-     *
-     * @return User
-     */
-    public function addLater(Later $later)
-    {
-        $this->laters[] = $later;
-
-        return $this;
-    }
-
-    /**
-     * Remove userFeed
-     *
-     */
-    public function removeLater(Later $later)
-    {
-        $this->laters->removeElement($later);
-    }
-
-    /**
-     * Get Laters
-     *
-     * @return Collection
-     */
-    public function getLaters()
-    {
-        return $this->laters;
     }
 }
