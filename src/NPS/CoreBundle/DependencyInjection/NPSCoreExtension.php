7f1ccc12939dc4b5ffaca664de9e5ed5eb4306a8
Denys Pasishnyi <dpcat237@gmail.com>
2013-09-07 18:20:13 +0000

    Recover previous version of DependencyInjections

--- src/NPS/CoreBundle/DependencyInjection/NPSCoreExtension.php
+++ src/NPS/CoreBundle/DependencyInjection/NPSCoreExtension.php
@@ -2,11 +2,29 @@
 
 namespace NPS\CoreBundle\DependencyInjection;
 
-use NPS\CoreBundle\DependencyInjection\AbstractNPSExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * NPSCoreExtension
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class NPSCoreExtension extends AbstractNPSExtension
+class NPSCoreExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     * load NPSCoreExtension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        // load NPSCoreBundle services
+        $loader->load('services.yml');
+    }
 }
--- src/NPS/FrontendBundle/DependencyInjection/NPSFrontendExtension.php
+++ src/NPS/FrontendBundle/DependencyInjection/NPSFrontendExtension.php
@@ -2,11 +2,29 @@
 
 namespace NPS\FrontendBundle\DependencyInjection;
 
-use NPS\CoreBundle\DependencyInjection\AbstractNPSExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * NPSFrontendExtension
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class NPSFrontendExtension extends AbstractNPSExtension
+class NPSFrontendExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     * load NPSFrontendExtension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        //load NPSFrontendBundle services
+        $loader->load('services.yml');
+    }
 }
