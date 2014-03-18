fb8bcf2779dd48888ebf18309d6e806532d46075
Denys Pasishnyi <dpcat237@gmail.com>
2014-03-18 18:46:09 +0100

    Fix name in comment

--- src/NPS/FrontendBundle/Resources/public/js/welcome.js
+++ src/NPS/FrontendBundle/Resources/public/js/welcome.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 
-/*Twotter*/
+/*Twitter*/
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 /*Facebook*/
