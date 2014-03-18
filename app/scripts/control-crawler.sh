7ffb4d607ca30cf46e1526355ae5148446bbb465
Denys Pasishnyi <dpcat237@gmail.com>
2014-03-18 19:50:52 +0100

    Run crawling in prod envirement

--- app/scripts/control-crawler.sh
+++ app/scripts/control-crawler.sh
@@ -2,6 +2,6 @@
 if ps -ef | grep -v grep | grep item:crawling ; then
     exit 0
 else
-    /var/www/nps/app/console item:crawling &
+    /var/www/nps/app/console item:crawling -e prod &
     exit 0
 fi
